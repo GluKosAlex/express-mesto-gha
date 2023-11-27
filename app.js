8ca561a68a7422ef6b7c55665d80d2c422c0c006
GluKosAlex <glukosalex@gmail.com>
2023-11-28 01:40:54 +0700

    feat(app): enable helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express, { json } from 'express';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import helmet from 'helmet';
 import router from './routes/index.js';
 
 const { PORT = 3000, CONN_STR = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
@@ -8,6 +9,8 @@ const { PORT = 3000, CONN_STR = 'mongodb://127.0.0.1:27017/mestodb' } = process.
 mongoose.connect(CONN_STR).then(() => console.log('Connection to the DB is successful'));
 
 const app = express();
+
+app.use(helmet());
 
 app.use(json());
 
